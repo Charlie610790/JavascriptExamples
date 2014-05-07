2baca483e4c4f437d9fc4b1c85ffc0d99ad1ff73
Charlie Jones <myupstateproperties@gmail.com>
2014-05-07 14:10:10 -0400

    adding player names and stats

--- main.js
+++ main.js
@@ -7,4 +7,12 @@ var x = 3
 var teamname = "Red Sox"
 // when I enter "teamname" in console it will pull teamname "Red Sox"
 
+// assigns team stats
+var teamstats = [15, 25, 50]
+//
+
+//assigning player variable names and values
+var bigPapi = {name: "David Ortiz", battingaverage: ".241", rbi: "17", homeruns: "6"}
+var pedroia = {name: "Dustin Pedroia", battingaverage: ".275", rbi: "10", homeruns: "1"}
+var napoli  = {name: "Mike Napoli", battingaverage: ".290", rbi: "17", honeruns: "5"}
 //
