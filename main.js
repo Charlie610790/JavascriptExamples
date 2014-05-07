1d008a5e7d6fa66aef03796e9ec9c028440ecb3f
Charlie Jones <myupstateproperties@gmail.com>
2014-05-07 13:52:28 -0400

    add more examples

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
-
 // simple variable that assigns the value of X to 3
 var x = 3
 // when run "x > 4" in console will return false
 
+// assigns variable teamname to "Red Sox"
+var teamname = "Red Sox"
+// when I enter "teamname" in console it will pull teamname "Red Sox"
+
+//
