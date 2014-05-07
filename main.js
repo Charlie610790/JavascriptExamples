506b0be1ff3db90fac78327112dfa9d9a272f75a
Charlie Jones <myupstateproperties@gmail.com>
2014-05-07 15:04:56 -0400

    add multiple new functions and update html to include baseball pulls of stats

--- main.js
+++ main.js
@@ -12,13 +12,49 @@ var teamstats = [15, 25, 50]
 //
 
 //assigning player variable names and values
-var bigPapi = {name: "David Ortiz", battingaverage: ".241", rbi: "17", homeruns: "6", singles: "100", doubles: "200"}
-var pedroia = {name: "Dustin Pedroia", battingaverage: ".275", rbi: "10", homeruns: "1", singles: "100", doubles: "200"}
-var napoli  = {name: "Mike Napoli", battingaverage: 0.290, rbi: "17", honeruns: "5", singles: "100", doubles: "200"}
+var ortiz = {name: "David Ortiz", battingaverage: 0.241, rbi: 17, homeruns: 6, doubles: 5, triples: 0, atbats: 112, games: 30}
+var pedroia = {name: "Dustin Pedroia", battingaverage: 0.275, rbi: 10, homeruns: 1, doubles: 11, triples: 0, atbats: 131, games: 31}
+var napoli  = {name: "Mike Napoli", battingaverage: 0.290, rbi: 17, homeruns: 5, doubles: 6, triples: 0, atbats: 107, games: 30}
 //
 
-function hitstotal (player) {
-	return player.singles  + player.doubles;
+// Function to pull all multiple base hits and add them together.
+function multiplebase (player) {
+	return player.doubles  + player.triples + player.homeruns;
 }
+//
 
-console.log('bigPapis total hits:', hitstotal(bigPapi) );
+// Function to pull player at bats and divide by the number of games to determine average at bat per game.
+function batspergame (player) {
+	return player.atbats / player.games;
+}
+//
+
+// Function to pull player homeruns and divide by the number of games to determine average home runs per game.
+function homerunspergame (player) {
+	return player.homeruns / player.games;
+}
+//
+
+// Function to pull player doubles and divide by the number of games to determine average home runs per game.
+function doublespergame (player) {
+	return player.doubles / player.games;
+}
+//
+
+// Function to pull player triples and divide by the number of games to determine average home runs per game.
+function triplespergame (player) {
+	return player.triples / player.games;
+}
+//
+
+
+
+
+
+
+
+
+
+console.log('Dustin Pedroia At Bats Per Game:', batspergame(pedroia));
+console.log('David Ortiz Multiples Base Hits:', multiplebase(ortiz));
+console.log('Mike Napoli Home Runs Per Game:', homerunspergame(napoli));
